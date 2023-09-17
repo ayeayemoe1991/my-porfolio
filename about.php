174a8f0868aea5ebed922a8e06de9acb73d799b6
Your Name <ayeayemoesan99@gmail.com>
2023-09-18 01:11:13 +0630

    my very fist commit

---
+++ about.php
@@ -0,0 +1,10 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<div class="row">
+    <div class="col-2"></div>
+    <div class="about col-8 my-5">
+        <h2>About Me</h2>
+        <p>My name is Chase Ohlson. I'm a frontend software engineer & freelance web developer in Los Angeles -- with roots in digital marketing & SEO.Experienced Junior web developer with 1+ years of experience in developing and maintaining e-learning website. SpeciaIized in PHP, Laravel and other frontend technoligies. I have a passion about web development trends and committed to enhancing the user experience through continuous learning.</p>
+    </div>
+    <div class="col-2"></div>
+</div>
